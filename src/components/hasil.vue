387a98797d3201808e160fcca329b52ea905a5c0
nurainigs04 <nurainigusnitasari@student.uir.ac.id>
2025-05-06 09:43:03 +0700

    klik

--- src/components/hasil.vue
+++ src/components/hasil.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app">
     <h1>📝 Daftar Kegiatan Sehari-hari</h1>
+
+    <form @submit.prevent="tambahKegiatan">
+      <input v-model="kegiatanBaru" placeholder="Tambah kegiatan..." />
+      <input v-model="jamBaru" type="time" />
+      <button type="submit">➕ Tambah</button>
+    </form>
+
   </div>
 </template>
 
