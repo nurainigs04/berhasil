c5a4308a332635c7a82ef280dbdcb8827d09d6de
nurainigs04 <nurainigusnitasari@student.uir.ac.id>
2025-05-06 09:39:13 +0700

    list

--- src/components/hasil.vue
+++ src/components/hasil.vue
@@ -1,10 +1,17 @@
-<script setup>
+<template>
+  <div class="app">
+    <h1>📝 Daftar Kegiatan Sehari-hari</h1>
+  </div>
+</template>
 
+<script setup>
+import { ref } from 'vue'
+
+const kegiatanBaru = ref('')
+const jamBaru = ref('')
+const daftarKegiatan = ref([])
 </script>
 
-<template>
-  
-</template>
 
 <style scoped>
 
